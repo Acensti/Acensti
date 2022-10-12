1a2a381f3a6447f2dc1a4ff3f4550ee95cbe385e
Acensti <37810260+Acensti@users.noreply.github.com>
2022-10-12 19:48:05 +0200

    cock
    new changes

---
+++ Shootyshot/Ai.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shootyshot
+{
+    public class Ai
+    {
+        public enum Action
+        {
+            Reload,
+            Shoot,
+            Block,
+            Shotgun,
+        }
+        public AI()
+        {
+            character = new Character();
+        }
+        public Character character;
+        public int AIAmmo = 0;
+       
+        public Action AiActions() //Mechanical choices the ai commits
+        {
+            if (AIAmmo == 3)
+            {
+                return Action.Shotgun;
+            }
+            else if (AIAmmo == 0)
+            {
+                Random rnd = new Random();
+                int number = rnd.Next(1, 2);
+                switch (number)
+                {
+                    case 1:
+                        AIAmmo++;
+                        return Action.Reload;
+                    case 2:
+                        return Action.Block;
+                }
+                return Action.Block;
+
+
+            }
+            else
+            {
+                Random rnd = new Random();
+                int number = rnd.Next(1, 3);
+                switch (number)
+                {
+                    case 1:
+                        AIAmmo++;
+                        return Action.Reload;
+                    case 2:
+                        return Action.Block;
+                    case 3:
+                        AIAmmo--;
+                        return Action.Shoot;
+                }
+
+                return Action.Block;
+            }
+
+
+        }
+    }
+}
---
+++ Shootyshot/Logic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shootyshot
+{
+    public class Logic
+    {
+        public enum Action
+        {
+            Reload,
+            Shoot,
+            Block,
+            Shotgun,
+        }
+
+        private void OngoingActions(Action AiChoices, Action PlayerAction)
+        {
+
+            if (AiChoices == Action.Shotgun)
+            {
+                //Game over
+            }
+
+            if (PlayerAction == Action.Shoot && AiChoices != Action.Block)
+            {
+                // Player win
+            }
+
+            if (PlayerAction != Action.Block && AiChoices == Action.Shoot)
+            {
+                //
+            }
+
+
+        }
+    }
+}
